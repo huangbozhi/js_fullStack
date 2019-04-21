2309d1e5cec4a370f8b5fe6e92b7eaf9025bc5ea
huangbozhi <1044213015@qq.com>
2019-04-21 09:00:03 +0800

    weApp and sort array

---
+++ wxApp/LOL/pages/legends/legends.js
@@ -0,0 +1,31 @@
+Page({
+    data:{
+        legends:[
+            {
+                name: '阿卡丽',
+                title: '刺客',
+                src: 'https://ossweb-img.qq.com/images/lol/img/champion/Akali.png'
+            },
+            {
+                name:'压缩',
+                title: '刺客',
+                src: 'https://ossweb-img.qq.com/images/lol/img/champion/Yasuo.png'
+            },
+            {
+                name: '瑞文',
+                title: '战士',
+                src: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big92004.jpg'
+            },
+            {
+                name: '卡特',
+                title: '法师',
+                src: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big55002.jpg'
+            },
+            {
+                name: '轮子妈',
+                title: 'adc',
+                src: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big15002.jpg'
+            }
+        ]
+    }
+})
