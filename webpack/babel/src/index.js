da376aa2e225f5040dd076c37bc66eeb70c6af9d
huangbozhi <1044213015@qq.com>
2019-05-21 15:50:47 +0800

    webpack

---
+++ webpack/babel/src/index.js
@@ -0,0 +1,9 @@
+const a = 1, b = 2, c = a + b;
+console.log(c);
+
+// [1, 2, 3].map(item => `${item} item`)
+
+// let flz = { name: '大妞' , hometown: "吉林"}
+// flz = { ...flz, "company": '新浪' }
+
+// console.log(flz)
