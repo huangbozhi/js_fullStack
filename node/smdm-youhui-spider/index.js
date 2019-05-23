c93cf01c872f92a1acb07a4451c54baa4dc82d6b
huangbozhi <1044213015@qq.com>
2019-05-23 19:12:17 +0800

    爬虫，and 柯里化

---
+++ node/smdm-youhui-spider/index.js
@@ -0,0 +1,72 @@
+const request = require('request');
+const cheerio = require('cheerio')
+
+function a(){
+    request("https://www.smzdm.com/youhui/", (err, res) => {
+        if(!err){
+            // console.log(res.body);
+            // decodeEntities 要不要解析 html entity
+            const $ = cheerio.load(res.body, {
+                decodeEntities: false
+            })
+            $('.list.list_preferential').each( function () {
+                let title = $('.listTitle a', this).html();
+                // <span></span>
+                title = title.replace(/<.*>.*<\/.*>/g, '');
+                const price = $('.listTitle .red', this).text();
+                const img = $('.picLeft img', this).attr('src');
+                const desc = $('.lrInfo', this).text().trim()
+                console.log({
+                    title,price,img, desc
+                })
+            })
+        }
+        
+    })
+}
+
+const url = 'http://lol.qq.com/v/v2/'
+function b(){
+    request(url, (err, res) => {
+      if(err){
+        // reject(err)
+        console.log(err)
+      } 
+      if(!err){
+        // console.log(res.body)
+        const $ = cheerio.load(res.body, {
+            decodeEntities: false
+        })
+        console.log(res.body)
+        $('.clearfix.mod-list.rec-video').each( function() {
+            const a = $('.mod-item .figure', this).attr('href');
+            console.log(a)
+        })
+      }
+    })
+}
+b()
+var iconv = require('iconv-lite')
+request({
+  url,
+  encoding: null
+}, (err, res) => {
+  if (err) {
+    // reject(err)
+    console.log(err)
+  }
+  if (!err) {
+    const body = iconv.decode(res.body, 'gbk');
+    const $ = cheerio.load(body, {
+      decodeEntities: false
+    })
+    console.log(body);
+    $('.clearfix.mod-list.rec-video').each(function () {
+      const a = $('.mod-item .figure', this).attr('href');
+      console.log(a)
+    })
+  }
+})
+
+
+
