a6a2babba25c02e308e3af9cf389e4ef6f9040ec
huangbozhi <1044213015@qq.com>
2019-06-05 14:16:42 +0800

    vue

---
+++ vue/vue-router/router/src/components/LookOne.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="look">
+    {{message}}
+    <p>接收的参数: {{$route.params.name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Look',
+  data () {
+    return {
+      message: '你瞅啥'
+    }
+  }
+}
+</script>
---
+++ vue/vue-router/router/src/components/LookTwo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="look">
+    {{message}}
+    <p>接收的参数: {{$route.params.userId}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Look',
+  data () {
+    return {
+      message: '瞅你咋地'
+    }
+  }
+}
+</script>
