20123210c606c2433bf5012ba234622b72cb6539
huangbozhi <1044213015@qq.com>
2019-06-11 19:00:35 +0800

    vue-iview npm

---
+++ vue/do-test/hbz-ui/README.md
@@ -0,0 +1,1 @@
+vue init webpack-simple hbz-ui
--- vue/vue-iview/iview/src/router/index.js
+++ vue/vue-iview/iview/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Home from '@/components/Home'
+import Index from '@/components/Index'
 
 Vue.use(Router)
 
@@ -10,6 +12,18 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/table',
+      name: 'Home',
+      component: Home,
+      children: [
+        {
+          path: '',
+          name: 'Index',
+          component: Index
+        }
+      ]
     }
   ]
 })
