0a1b5a5af548a213a794bcdf111a1efb57cfd8d8
huangbozhi <1044213015@qq.com>
2019-06-21 14:37:53 +0800

    vuex-counter

---
+++ vue/vuex-counter/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- <counter :count="count" v-on:increment="incrementCount" /> -->
+    <counter  />
+    <!-- <counter-borther :count="count" /> -->
+    <counter-borther />
+  </div>
+</template>
+
+<script>
+// 两个组件都要用到count 状态，怎么办
+import Counter from './components/Counter'
+import CounterBrother from './components/CounterBrother'
+
+export default {
+  name: 'App',
+  components: {
+    'counter': Counter,
+    'counter-borther': CounterBrother,
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vuex-counter/src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{$store.state.count}} times,
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['count'],
+  methods: {
+    increment () {
+      // this.$emit('increment');
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
